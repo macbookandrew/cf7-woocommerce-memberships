b7744c30a4fb1ac0b3fa9f82de6280866e9d67e2
Andrew Minion <andrew@andrewrminion.com>
2018-06-05 22:28:03 -0400

    fix phpunit test path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/cf7-woo-memberships.php';
+	require dirname( dirname( __FILE__ ) ) . '/cf7-woocommerce-memberships.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
