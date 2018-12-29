513ec751c5a8d3c849b49b767ece263a14603369
Andrew Minion <andrew@andrewrminion.com>
2018-12-29 14:28:33 -0500

    update author name

--- cf7-woocommerce-memberships.php
+++ cf7-woocommerce-memberships.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Contact Form 7 → WooCommerce Memberships
  * Plugin URI:      https://andrewrminion.com/2018/04/cf7-woo-memberships/
  * Description:     Adds Contact Form 7 entries as new users (if necessary) and grants them the specified WooCommerce Membership plan.
- * Author:          AndrewRMinion Design
+ * Author:          Andrew Minion
  * Author URI:      https://andrewrminion.com/
  * Text Domain:     cf7-woo-memberships
  * Domain Path:     /languages
